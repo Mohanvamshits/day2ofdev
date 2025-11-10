c3d419407ce046a5b48845ae1a3df4cb882766ec
Mohanvamshits <tsmohanv6@gmail.com>
2025-11-10 19:16:11 +0530

    Add additional print statement to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to the Devops in CloudDevopshub!");
+        System.out.println("Day4-jenkins CICD class")
     }
+
 }   
